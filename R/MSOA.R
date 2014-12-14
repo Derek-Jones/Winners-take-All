181b6f7f505cd10ce6355d93530cbc9adec522d9
Pavel Reich <preich@Pavels-MacBook-Pro.local>
2014-12-14 12:49:11 +0000

    updated

--- R/MSOA.R
+++ R/MSOA.R
@@ -13,19 +13,18 @@ addr <- read.csv(paste("T", month, "ADDR_BNFT.CSV",sep=""), header=F)
 addr <- addr[,c("V2","V8")]
 names(addr) <- c("PRACTICE","postcode")
 addr$postcode <- fixPostCode(addr$postcode)
-#addr$postcode <- gsub("\\s*$", "", addr$postcode)
-#addr$postcode <- gsub("\\s+", " ", addr$postcode)
 
 income <- read.csv("income-per-msoa-postcode.csv")
-#income$postcode <- gsub("\\s+", " ", income$postcode)
 income$postcode <- fixPostCode(income$postcode)
 practices <- sqldf("select PRACTICE, sum(NIC) as NIC, count(*) as prescriptions, sum(ITEMS) as ITEMS, avg(generic) as generic_ratio from pdpi group by PRACTICE")
-#practices <- aggregate(NIC ~ PRACTICE, pdpi, sum)
 
 m <- merge(merge(addr, practices), income)
 population <- read.csv("population.csv")
 pop <- population[,c("MSOA","residents")]
 g <- merge(m, pop)
-nrow(g)
 ms <- sqldf("select MSOA, residents, gross_income*52 as annual_income, sum(NIC) as NIC, sum(ITEMS) as ITEMS_SUM, sum(ITEMS)/residents as ITEMS_per_capita, sum(prescriptions)/residents as prescriptions_per_capita, sum(prescriptions) as prescriptions, sum(NIC)/residents as NIC_per_capita, avg(generic_ratio) as avg_generic_ratio, min(generic_ratio) as min_generic_ratio, max(generic_ratio) as max_generic_ratio, count(distinct(PRACTICE)) as practices, residents/count(distinct(PRACTICE)) as residents_per_practice from g group by MSOA, residents, gross_income")
+ms$prescriptions_per_capita <- round(ms$prescriptions/ms$residents,3)
+ms$ITEMS_per_capita <- round(ms$ITEMS_SUM/ms$residents,3)
+ms$avg_generic_ratio <- round(ms$avg_generic_ratio, 3)
+ms$NIC_per_capita <- round(ms$NIC_per_capita,3)
 write.csv(ms, paste(month,"NIC-MSOA-practices.csv",sep=""))
--- R/charts.R
+++ R/charts.R
@@ -6,13 +6,12 @@ fname <- paste("../data/",month,"NIC-MSOA-practices.csv",sep="")
 data <- read.csv(fname)
 data$prescriptions_per_capita <- data$prescriptions/data$residents
 library(ggplot2)
-
+monthName <- gsub("([0-9]{4})([0-9]{2})","\\1-\\2",month)
 generatePlot <- function(param,ptext,ytext) {
-  p <- ggplot(data, aes_string(x="annual_income", y=param)) + stat_smooth() + theme_bw() + ggtitle(ptext) +xlab("Median annual income per capita in MSOA") + ylab(ytext)
+  p <- ggplot(data, aes_string(x="annual_income", y=param)) + stat_smooth() + theme_bw() + ggtitle(paste(monthName,ptext,sep=": ")) +xlab("Average household annual income in MSOA") + ylab(ytext)
   ggsave(p, filename=paste(month,"_",param,".png",sep=""))
 }
 generatePlot("prescriptions_per_capita", "Prescriptions per capita", "Prescriptions per capita")
-generatePlot("NIC_per_capita", "Drug expenses per capita", "Pounds per capita")
+generatePlot("NIC_per_capita", "Prescription costs per capita", "Pounds per capita")
 generatePlot("residents_per_practice", "Residents per practice", "Residents")
-generatePlot("avg_generic_ratio", "Ratio of generic drugs in prescriptions", "Residents")
-
+generatePlot("avg_generic_ratio", "Ratio of generic drugs in prescriptions", "Ratio")
